3762c779e79ca549d21e3af2a357045ecd375e30
rtenreiro <tenreiroramon@gmail.com>
2013-07-15 12:29:14 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs  = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html','utf-8'));
+  var html = fs.readFileSync(htmlfile).to string();
+  response.send(html);
 });
 
 var port = process.env.PORT || 8080;
-
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
